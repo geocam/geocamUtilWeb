09723ef787efbffd26e78bcf65f4a93d26d2d364
trey.smith@nasa.gov <trey.smith@nasa.gov>
2011-02-04 15:56:39 -0800

    added models.UuidField, models.ExtrasField, forms.UuidField, anyjson

---
+++ geocamUtil/models/UuidField.py
@@ -0,0 +1,36 @@
+# __BEGIN_LICENSE__
+# Copyright (C) 2008-2010 United States Government as represented by
+# the Administrator of the National Aeronautics and Space Administration.
+# All Rights Reserved.
+# __END_LICENSE__
+
+try:
+    import uuid
+except ImportError:
+    uuid = None
+
+from django.db import models
+
+if uuid:
+    def makeUuid():
+        return str(uuid.uuid4())
+else:
+    import random
+    def makeUuid():
+        return '%04x-%02x-%02x-%02x-%06x' % (random.getrandbits(32), random.getrandbits(8),
+                                             random.getrandbits(8), random.getrandbits(8),
+                                             random.getrandbits(48))
+
+class UuidField(models.CharField):
+    def __init__(self, *args, **kwargs):
+        kwargs.setdefault('max_length', 48)
+        kwargs.setdefault('editable', False)
+        super(UuidField, self).__init__(self, *args, **kwargs)
+        
+    def pre_save(self, model_instance, add):
+        if add:
+            value = makeUuid()
+            setattr(model_instance, self.attname, value)
+            return value
+        else:
+            return super(UuidField, self).pre_save(model_instance, add)
