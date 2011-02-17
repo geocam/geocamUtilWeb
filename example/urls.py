765f3804188da445668670f0609cd9ed4796794c
trey.smith@nasa.gov <trey.smith@nasa.gov>
2011-02-16 16:11:25 -0800

    added "example" django site to support running django-specific tests

---
+++ example/urls.py
@@ -0,0 +1,33 @@
+# __BEGIN_LICENSE__
+# Copyright (C) 2008-2010 United States Government as represented by
+# the Administrator of the National Aeronautics and Space Administration.
+# All Rights Reserved.
+# __END_LICENSE__
+
+from django.conf.urls.defaults import *
+from django.conf import settings
+
+# Uncomment the next two lines to enable the admin:
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Example:
+    # (r'^example/', include('example.foo.urls')),
+
+    # Uncomment the admin/doc line below and add 'django.contrib.admindocs' 
+    # to INSTALLED_APPS to enable admin documentation:
+    # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    (r'^admin/', include(admin.site.urls)),
+    
+)
+
+urlpatterns = urlpatterns + patterns('',
+    (r'^static/(?P<path>.*)$', 'django.views.static.serve',
+        {'document_root': settings.MEDIA_ROOT}),
+#    (r'^data/(?P<path>.*)$', 'django.views.static.serve',
+#        {'document_root': settings.DATA_ROOT}),
+    )
+
