8e7c6f9098baa9ec8240de4750843d4eac43353e
trey.smith@nasa.gov <trey.smith@nasa.gov>
2011-02-25 19:10:32 -0800

    improved Builder use of logging

--- geocamUtil/Builder.py
+++ geocamUtil/Builder.py
@@ -14,6 +14,7 @@ class Builder:
     def __init__(self):
         self.numMade = 0
         self.numRules = 0
+        self.logger = logging.getLogger('geocamUtil.Builder')
 
     def applyRule(self, dst, srcs, func):
         self.numRules += 1
@@ -30,7 +31,7 @@ class Builder:
             dstMode = dstStat[stat.ST_MODE]
             if stat.S_ISLNK(dstMode) or stat.S_ISDIR(dstMode):
                 # assume symlinks and directories are up to date
-                logging.debug('builder: %s is symlink or directory, no rebuild needed' % dst)
+                self.logger.debug('builder: %s is symlink or directory, no rebuild needed' % dst)
                 rebuild = False
             else:
                 dstTime = dstStat[stat.ST_MTIME]
@@ -40,22 +41,22 @@ class Builder:
                         srcTime = os.stat(src)[stat.ST_MTIME]
                     except OSError, e:
                         traceback.print_exc()
-                        logging.error('[could not stat source file %s in rule to generate %s]'
+                        self.logger.error('[could not stat source file %s in rule to generate %s]'
                                       % (src, dst))
                         sys.exit(1)
                     maxSrcTime = max(maxSrcTime, srcTime)
-                logging.debug('builder: srcs=%s maxSrcTime=%s dstTime=%s'
+                self.logger.debug('builder: srcs=%s maxSrcTime=%s dstTime=%s'
                               % (srcs, maxSrcTime, dstTime))
                 rebuild = (maxSrcTime > dstTime)
         else:
             rebuild = True
 
         if rebuild:
-            logging.debug('[building: %s]' % dst)
+            self.logger.debug('[building: %s]' % dst)
             func()
             self.numMade += 1
         else:
-            logging.debug('[up to date: %s]' % dst)
+            self.logger.debug('[up to date: %s]' % dst)
 
     def finish(self):
-        logging.info('builder: %d of %d files were up to date' % (self.numRules - self.numMade, self.numRules))
+        self.logger.info('builder: %d of %d files were up to date' % (self.numRules - self.numMade, self.numRules))
