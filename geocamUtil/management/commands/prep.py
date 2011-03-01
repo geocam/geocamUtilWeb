469722506bae7be905ab24a0cde400a3aaf1d7e3
trey.smith@nasa.gov <trey.smith@nasa.gov>
2011-03-01 07:52:41 -0800

    added site-level prep.py

---
+++ geocamUtil/management/commands/prep.py
@@ -0,0 +1,17 @@
+# __BEGIN_LICENSE__
+# Copyright (C) 2008-2010 United States Government as represented by
+# the Administrator of the National Aeronautics and Space Administration.
+# All Rights Reserved.
+# __END_LICENSE__
+
+from django.core import management
+from django.core.management.base import NoArgsCommand
+
+class Command(NoArgsCommand):
+    help = 'Runs subcommands: collectreqs, installreqs, prepapps, collectmedia'
+    
+    def handle_noargs(self, **options):
+        management.call_command('collectreqs')
+        management.call_command('installreqs')
+        management.call_command('prepapps')
+        management.call_command('collectmedia')
