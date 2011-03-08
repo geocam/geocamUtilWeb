640570757e0bc8c13276cb1ec6cda5e9889d2f3e
trey.smith@nasa.gov <trey.smith@nasa.gov>
2011-03-08 14:39:31 -0800

    added getIconUrl() to geocamUtil.icons

--- geocamUtil/icons/__init__.py
+++ geocamUtil/icons/__init__.py
@@ -5,21 +5,28 @@
 # __END_LICENSE__
 
 import os
+import re
 from glob import glob
 
 from PIL import Image
+from django.conf import settings
 
+ICON_URL_CACHE = {}
 ICON_SIZE_CACHE = {}
 
-def cacheIconSize(dir):
+def cacheIcons(dir, mediaDir=settings.MEDIA_ROOT, mediaUrl=settings.MEDIA_URL):
     paths = glob('%s/*' % dir)
     for p in paths:
         iconPrefix = os.path.splitext(os.path.basename(p))[0]
         im = Image.open(p)
         ICON_SIZE_CACHE[iconPrefix] = list(im.size)
+        ICON_URL_CACHE[iconPrefix] = re.sub(mediaDir, mediaUrl, p)
 
 def getIconSize(iconPrefix):
     return ICON_SIZE_CACHE[iconPrefix]
+
+def getIconUrl(iconPrefix):
+    return ICON_URL_CACHE[iconPrefix]
 
 # for export
 import rotate
