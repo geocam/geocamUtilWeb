2b462a33f6f25460d2a1a4eed8ce4b51262af701
trey.smith@nasa.gov <trey.smith@nasa.gov>
2011-03-08 18:00:00 -0800

    moved lots of util files from geocamCore to geocamUtil

---
+++ geocamUtil/FileUtil.py
@@ -0,0 +1,44 @@
+# __BEGIN_LICENSE__
+# Copyright (C) 2008-2010 United States Government as represented by
+# the Administrator of the National Aeronautics and Space Administration.
+# All Rights Reserved.
+# __END_LICENSE__
+
+import os
+import glob
+import re
+import errno
+import datetime
+import time
+import tempfile
+
+import pytz
+
+from django.conf import settings
+
+class NoDataError(Exception):
+    pass
+
+def importModuleByName(name):
+    mod = __import__(name)
+    components = name.split('.')
+    for comp in components[1:]:
+        mod = getattr(mod, comp)
+    return mod
+
+def getMiddleFileWithExtension(ext, path):
+    allMatches = glob.glob('%s/*.%s' % (path, ext))
+    allMatches = [x for x in allMatches
+               if not x.startswith('thumbnail')]
+    if not allMatches:
+        raise NoDataError('no %s files in %s' % (ext, path))
+    allMatches.sort()
+    assert len(allMatches) > 0
+    return allMatches[len(allMatches)//2]
+    
+def mkdirP(dir):
+    try:
+        os.makedirs(dir)
+    except OSError, err:
+        if err.errno != errno.EEXIST:
+            raise
