34f3dfec280a73d11711e228e34e79ae8feef308
Dave Jackson <dave.jackson@nasa.gov>
2011-07-05 16:58:35 -0700

    Added Python flash policy server

---
+++ geocamUtil/wsflashpolicy.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python
+
+import optparse
+import sys
+import signal
+import socket
+import SocketServer
+
+class FlashPolicyServer(SocketServer.BaseRequestHandler):    
+    def handle(self):
+        policyText = '<?xml version="1.0"?><cross-domain-policy><allow-access-from domain="%s" to-ports="%s" /></cross-domain-policy>' % (FlashPolicyServer.allowedDomain, FlashPolicyServer.allowedPort)
+        
+        self.data = self.request.recv(1024).strip()
+        print "%s wrote:" % self.client_address[0]
+        print self.data
+        #self.allowedDomain = '*'
+        #self.allowedPort = '*'
+        if '<policy-file-request/>' in self.data:
+            print 'received policy request'
+            print 'sending policy: %s' % policyText
+            self.request.send(policyText)
+
+class Usage(Exception):
+    def __init__(self,msg):
+        self.msg = msg
+
+def main():
+    try:
+        usage = 'wsflashpolicy.py -b bind-host [-p bind-port] -d allowed-domains -o allowed-ports'
+        parser = optparse.OptionParser(usage=usage)
+        parser.add_option('-b', '--bind-host', dest='bhost', help="Host to which server should bind")
+        parser.add_option('-p', '--bind-port', dest='bport', help="Port to which server should bind")
+        parser.add_option('-d', '--allowed-domains', dest='ahosts', help="List of domains to which flash is allowed to connect")
+        parser.add_option('-o', '--allowed-ports', dest='aports', help="List of ports to which flash is allowed to connect")
+        (options, args) = parser.parse_args()
+    except optparse.OptionError, msg:
+        raise Usage(msg)
+
+    if options.bhost:
+        bhost = options.bhost
+    else:
+        bhost = ''
+
+    if options.bport:
+        bport = options.bport
+    else:
+        bport = 843
+
+    if options.ahosts:
+        ahosts = options.ahosts
+    else:
+        ahosts = ''
+
+    if options.aports:
+        aports = options.aports
+    else:
+        aports = ''
+
+    FlashPolicyServer.allowedDomain = ahosts
+    FlashPolicyServer.allowedPort = aports
+
+    server = SocketServer.TCPServer((bhost, bport), FlashPolicyServer)
+
+    def signal_handler(signal, frame):
+        print "Caught Ctrl+C, shutting down..."
+        sys.exit()
+    signal.signal(signal.SIGINT, signal_handler)
+
+    try:
+        server.serve_forever()
+    except KeyboardInterrupt:
+        sys.stdout.write('\r\n')
+        sys.exit()
+
+if __name__ == "__main__":
+    sys.exit(main())
