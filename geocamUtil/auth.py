42811010573195dce7c8383126262643ff58a2ff
Chris Corcoran <chris@thingsgood.com>
2011-07-06 14:21:19 -0700

    Updated the get_account_widget helper to output html for new layout

--- geocamUtil/auth.py
+++ geocamUtil/auth.py
@@ -8,13 +8,18 @@ from geocamUtil.middleware.SecurityMiddleware import requestIsSecure
 from geocamAware import settings
 import urllib
 
-def getAccountWidget(request):
+def get_account_widget(request):
     if request.user.is_authenticated():
         """
         accountWidget = ('<a href="%(SCRIPT_NAME)saccounts/profile">%(username)s</a> <a href="%(SCRIPT_NAME)saccounts/logout/">logout</a>'
                          % dict(SCRIPT_NAME=settings.SCRIPT_NAME, username=request.user.username))
         """
+        """
         accountWidget = ('<a href="%(SCRIPT_NAME)saccounts/profile">%(username)s</a> | <a href="%(SCRIPT_NAME)saccounts/logout/">Logout</a>'
+                        % dict(SCRIPT_NAME=settings.SCRIPT_NAME, username=request.user.username))
+        """
+                        
+        accountWidget = ('<div onclick="go_to_view(\'%(SCRIPT_NAME)saccounts/logout/\')"><a href="javascript:void(0);">Logout</a></div>'
                         % dict(SCRIPT_NAME=settings.SCRIPT_NAME, username=request.user.username))
         
     else:
@@ -27,8 +32,11 @@ def getAccountWidget(request):
                          % dict(SCRIPT_NAME=settings.SCRIPT_NAME,
                                 path=urllib.quote(path)))
         """
-        
-        accountWidget = ('<div id="accountwidget"><a id="login_button" href="%(SCRIPT_NAME)saccounts/login?next=%(path)s">Login</a> | <a id="join_button" href="%(SCRIPT_NAME)saccounts/register">Join</a></div>'
+        """
+        accountWidget = ('<div id="accountwidget"><a id="login_button" href="%(SCRIPT_NAME)saccounts/login/?next=%(path)s">Login</a> | <a id="join_button" href="%(SCRIPT_NAME)saccounts/register">Join</a></div>'
+                        % dict(path=request.path, SCRIPT_NAME=settings.SCRIPT_NAME, username=request.user.username))
+        """
+        accountWidget = ('<div onclick="go_to_view(\'%(SCRIPT_NAME)saccounts/login/?next=%(path)s\')"><a id="login_button" href="javascript:void(0);">Login</a></div> <div onclick="go_to_view(\'%(SCRIPT_NAME)saccounts/register/\')"><a id="join_button" href="javascript:void(0);">Join</a></div>'
                         % dict(path=request.path, SCRIPT_NAME=settings.SCRIPT_NAME, username=request.user.username))
                                 
     return accountWidget
