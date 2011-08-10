261c817d83e31f3be9f038f4d581713b8d3b081e
Trey Smith <trey.smith@nasa.gov>
2011-08-10 11:27:20 -0700

    prep.py: remove weird line-noise type stuff introduced in last commit

--- geocamUtil/management/commands/prep.py
+++ geocamUtil/management/commands/prep.py
@@ -1,4 +1,4 @@
-1;2600;0c# __BEGIN_LICENSE__
+# __BEGIN_LICENSE__
 # Copyright (C) 2008-2010 United States Government as represented by
 # the Administrator of the National Aeronautics and Space Administration.
 # All Rights Reserved.
