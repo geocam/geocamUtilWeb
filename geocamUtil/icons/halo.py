a4cddbcea06749b99a2114c658ff8ad4df1a35fe
Trey Smith <trey.smith@nasa.gov>
2011-08-21 09:41:14 -0700

    icons.halo: added halo width control

--- geocamUtil/icons/halo.py
+++ geocamUtil/icons/halo.py
@@ -4,9 +4,11 @@
 # All Rights Reserved.
 # __END_LICENSE__
 
+import os
+
 from PIL import Image, ImageOps, ImageFilter
 
-HALO_PAD_WIDTH=32
+DEFAULT_HALO_WIDTH = 0.1
 
 def autocrop(img):
     r, g, b, a = img.split()
@@ -21,44 +23,57 @@ def thresholdFunc(v, thresh):
 def thresholdImage(img, thresh):
     return img.point(lambda v: thresholdFunc(v, thresh))
 
-def doAddHalo(inPath, outPath, threshold):
+def doAddHalo(inPath, outPath, width):
     im = Image.open(inPath)
     im.load()
-    r, g, b, a = im.split()
-    paddedA = ImageOps.expand(a, border=HALO_PAD_WIDTH)
-    blur = paddedA.filter(ImageFilter.BLUR)
-    halo = thresholdImage(blur, threshold)
+    n = max(im.size)
+    widthPixels = int(n * width)
+    padded = ImageOps.expand(im, border=3*widthPixels)
+    _, _, _, paddedA = padded.split()
+
+    halo = paddedA.copy()
+    numBlurIterations = int(widthPixels/2.5 + 0.5)
+    for i in xrange(0, numBlurIterations):
+        halo = halo.filter(ImageFilter.BLUR)
+        halo = thresholdImage(halo, 20)
+        
     zero = Image.new('L', paddedA.size, 0)
     one = Image.new('L', paddedA.size, 256)
     result = Image.merge('RGBA', [one, zero, zero, halo])
-    padded = ImageOps.expand(im, border=HALO_PAD_WIDTH)
     result.paste(padded, None, padded)
+
+    outDir = os.path.dirname(os.path.abspath(outPath))
+    if not os.path.exists(outDir):
+        os.makedirs(outDir)
+
     autocrop(result).save(outPath)
 
-def addHalo(builder, inPath, outPath, threshold=30):
+def addHalo(builder, inPath, outPath, width=DEFAULT_HALO_WIDTH):
     """
     Runs a builder rule that reads the image at inPath, which must be an
     RGBA image, adds a red "halo" around the area of the image with
-    non-zero alpha, and writes the result to outPath. @threshold is a
-    crude control on the width of the halo (0-255, lower values make a
-    wider halo).
+    non-zero alpha, and writes the result to outPath. @width specifies
+    very roughly how wide the halo should be as a proportion of the image
+    size (0..1).
     """
-    builder.applyRule(outPath, [inPath], lambda: doAddHalo(inPath, outPath, threshold))
+    builder.applyRule(outPath, [inPath], lambda: doAddHalo(inPath, outPath, width))
 
 def main():
     import optparse
     parser = optparse.OptionParser('usage: %prog <in.png> <out.png>')
-    parser.add_option('-t', '--threshold',
-                      type=int, default=30,
-                      help='Smaller threshold value (0..255) gives a larger halo [%default]')
+    parser.add_option('-w', '--width',
+                      type=float, default=DEFAULT_HALO_WIDTH,
+                      help='Width of halo as a proportion of the image size (0..1) [%default]')
     opts, args = parser.parse_args()
     if len(args) != 2:
         parser.error('expected exactly 2 args')
+    if not (0 < opts.width and opts.width < 1):
+        parser.error('width must be in range 0..1')
 
     from geocamUtil.Builder import Builder
     builder = Builder()
 
-    addHalo(builder, args[0], args[1], opts.threshold)
+    addHalo(builder, args[0], args[1], opts.width)
 
 if __name__ == '__main__':
     main()
