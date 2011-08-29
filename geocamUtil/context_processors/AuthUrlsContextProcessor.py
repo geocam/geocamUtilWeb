8a6213de4caa7beaaee2e9b6aff2a1041234702b
Trey Smith <trey.smith@nasa.gov>
2011-08-29 07:37:51 -0500

    added login_url_with_next

--- geocamUtil/context_processors/AuthUrlsContextProcessor.py
+++ geocamUtil/context_processors/AuthUrlsContextProcessor.py
@@ -10,7 +10,13 @@ def AuthUrlsContextProcessor(request):
     """
     Adds login and logout urls to the context.
     """
+    if hasattr(settings, 'LOGIN_DEFAULT_NEXT_URL'):
+        loginSuffix = '?next=' + settings.LOGIN_DEFAULT_NEXT_URL
+    else:
+        loginSuffix = ''
+
     return {
         'login_url': settings.LOGIN_URL,
-        'logout_url': settings.LOGOUT_URL
+        'logout_url': settings.LOGOUT_URL,
+        'login_url_with_next': settings.LOGIN_URL + loginSuffix
     }
