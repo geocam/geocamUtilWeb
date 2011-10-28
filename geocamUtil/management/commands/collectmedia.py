66cc540ef1ec49a51fec9a7aad8ff4f10d704ea7
Trey Smith <trey.smith@nasa.gov>
2011-10-28 14:11:32 -0700

    collectmedia now installs admin media

--- geocamUtil/management/commands/collectmedia.py
+++ geocamUtil/management/commands/collectmedia.py
@@ -7,6 +7,8 @@
 import os
 import logging
 
+import django
+
 from geocamUtil.management import commandUtil
 from geocamUtil.Installer import Installer
 
@@ -15,6 +17,14 @@ class Command(commandUtil.PathCommand):
     help = 'Collect media from all apps into build/media'
 
     def handleImportPaths(self, impPaths, options):
+        inst = Installer()
+        siteDir = commandUtil.getSiteDir()
+
+        # install admin media
+        djangoDir = os.path.dirname(os.path.realpath(django.__file__))
+        adminMediaDir = os.path.join(djangoDir, 'contrib', 'admin', 'media')
+        inst.installRecurse(adminMediaDir, '%sbuild/media/admin' % siteDir)
+
         for impPath in impPaths:
             logging.debug('collectmedia app %s', impPath)
             appMod = __import__(impPath, fromlist=['dummy'])
@@ -24,7 +34,5 @@ class Command(commandUtil.PathCommand):
             for mediaPath in tryMediaPaths:
                 logging.debug('collectmedia app %s: checking for media in %s', impPath, mediaPath)
                 if os.path.exists(mediaPath):
-                    siteDir = commandUtil.getSiteDir()
-                    inst = Installer()
                     inst.installRecurseGlob('%s/*' % mediaPath, '%sbuild/media' % siteDir)
                     break
