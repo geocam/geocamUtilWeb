ae656081ae7277b19219dea3606aae64c15a0242
Trey Smith <trey.smith@nasa.gov>
2011-10-28 10:26:15 -0700

    fixed url escaping bug and improved style of getAccountWidget

--- geocamUtil/auth.py
+++ geocamUtil/auth.py
@@ -10,14 +10,19 @@ from geocamAware import settings
 
 def getAccountWidget(request):
     if request.user.is_authenticated():
-        accountWidget = ('<a href="%(SCRIPT_NAME)saccounts/profile">%(username)s</a> | <a href="%(SCRIPT_NAME)saccounts/logout/">Logout</a>'
+        accountWidget = ('<a href="%(SCRIPT_NAME)saccounts/profile">%(username)s</a>&nbsp;<a href="%(SCRIPT_NAME)saccounts/logout/">Logout</a>'
                         % dict(SCRIPT_NAME=settings.SCRIPT_NAME, username=request.user.username))
     else:
         path = request.get_full_path()
-        if not requestIsSecure(request):
-            path += '?protocol=http'  # redirect back to http after login
+        if requestIsSecure(request):
+            protocol = ''
+        else:
+            protocol = '&protocol=http'  # redirect back to http after login
 
-        accountWidget = ('<div id="accountwidget"><a id="login_button" href="%(SCRIPT_NAME)saccounts/login?next=%(path)s">Login</a> | <a id="join_button" href="%(SCRIPT_NAME)saccounts/register">Join</a></div>'
-                        % dict(path=path, SCRIPT_NAME=settings.SCRIPT_NAME, username=request.user.username))
+        accountWidget = ('<div id="accountwidget"><a id="login_button" href="%(SCRIPT_NAME)saccounts/login?next=%(path)s%(protocol)s">Login</a> | <a id="join_button" href="%(SCRIPT_NAME)saccounts/register">Join</a></div>'
+                        % dict(path=path,
+                               SCRIPT_NAME=settings.SCRIPT_NAME,
+                               username=request.user.username,
+                               protocol=protocol))
 
     return accountWidget
