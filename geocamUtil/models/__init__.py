fd0988fe9b2d37ed9530a05ee7d0e15d665741a8
Trey Smith <trey.smith@nasa.gov>
2012-05-23 13:03:15 +0000

    removed reference to not-yet-checked-in classes

--- geocamUtil/models/__init__.py
+++ geocamUtil/models/__init__.py
@@ -7,7 +7,7 @@
 from django.db import models
 
 from UuidField import UuidField
-from ExtrasField import ExtrasField, JsonTextField, JsonCharField
+from ExtrasField import ExtrasField #, JsonTextField, JsonCharField
 from dateTimeUtc import DateTimeUtcField
 from timestampDescriptor import TimestampDescriptor
 
