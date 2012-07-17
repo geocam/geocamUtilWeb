485ba3b69fc5dc6e354573dc2e985bdc9f728971
Trey Smith <trey.smith@nasa.gov>
2012-07-16 14:56:29 -1000

    added batch.py

---
+++ geocamUtil/batch.py
@@ -0,0 +1,24 @@
+# __BEGIN_LICENSE__
+# Copyright (C) 2008-2010 United States Government as represented by
+# the Administrator of the National Aeronautics and Space Administration.
+# All Rights Reserved.
+# __END_LICENSE__
+
+from collections import deque
+
+
+class BatchProcessor(object):
+    def __init__(self, func, n):
+        self.func = func
+        self.n = n
+        self.queue = deque()
+
+    def add(self, elt):
+        self.queue.append(elt)
+        if len(self.queue) >= self.n:
+            self.func(self.queue)
+            self.queue.clear()
+
+    def flush(self):
+        self.func(self.queue)
+        self.queue.clear()
