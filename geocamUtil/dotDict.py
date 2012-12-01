17db89dfcde19adbe74ee7236dddd57348fee043
Trey Smith <trey.smith@nasa.gov>
2012-12-01 19:41:17 +0000

    moved DotDict, convertToDotDictRecurse from models/ExtrasDotField.py to dotDict.py so they can be used without incurring a Django dependency

---
+++ geocamUtil/dotDict.py
@@ -0,0 +1,38 @@
+# __BEGIN_LICENSE__
+# Copyright (C) 2008-2010 United States Government as represented by
+# the Administrator of the National Aeronautics and Space Administration.
+# All Rights Reserved.
+# __END_LICENSE__
+
+from geocamUtil import anyjson as json
+
+
+def convertToDotDictRecurse(struct):
+    if isinstance(struct, dict):
+        for k, v in struct.iteritems():
+            struct[k] = convertToDotDictRecurse(v)
+        return DotDict(struct)
+    elif isinstance(struct, list):
+        return [convertToDotDictRecurse(elt) for elt in struct]
+    else:
+        return struct
+
+
+class DotDict(dict):
+    # At the moment this object exists pretty much solely to let you
+    # get and set elements in its __dict__ dictionary via dotted
+    # notation.  Someday it could do more.
+
+    # these are fields that must not be defined to avoid causing problems
+    # with Django
+    _badFields = ('prepare_database_save',)
+
+    def __repr__(self):
+        return json.dumps(self, sort_keys=True, indent=4)
+
+    def __getattr__(self, attr):
+        if attr in self._badFields:
+            raise KeyError(attr)
+        return self.get(attr, None)
+    __setattr__ = dict.__setitem__
+    __delattr__ = dict.__delitem__
--- geocamUtil/models/ExtrasDotField.py
+++ geocamUtil/models/ExtrasDotField.py
@@ -8,37 +8,7 @@ from django.db import models
 from django.core.exceptions import ValidationError
 
 from geocamUtil import anyjson as json
-
-
-def convertToDotDictRecurse(struct):
-    if isinstance(struct, dict):
-        for k, v in struct.iteritems():
-            struct[k] = convertToDotDictRecurse(v)
-        return DotDict(struct)
-    elif isinstance(struct, list):
-        return [convertToDotDictRecurse(elt) for elt in struct]
-    else:
-        return struct
-
-
-class DotDict(dict):
-    # At the moment this object exists pretty much solely to let you
-    # get and set elements in its __dict__ dictionary via dotted
-    # notation.  Someday it could do more.
-
-    # these are fields that must not be defined to avoid causing problems
-    # with Django
-    _badFields = ('prepare_database_save',)
-
-    def __repr__(self):
-        return json.dumps(self, sort_keys=True, indent=4)
-
-    def __getattr__(self, attr):
-        if attr in self._badFields:
-            raise KeyError(attr)
-        return self.get(attr, None)
-    __setattr__ = dict.__setitem__
-    __delattr__ = dict.__delitem__
+from geocamUtil.dotDict import DotDict, convertToDotDictRecurse
 
 
 class ExtrasDotField(models.TextField):
