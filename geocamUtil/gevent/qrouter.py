7eb13b8e3b1090c4f6e73f232c1d95fcc58498ce
Trey Smith <trey.smith@nasa.gov>
2013-10-09 11:39:38 -0700

    add gevent.qrouter

---
+++ geocamUtil/gevent/qrouter.py
@@ -0,0 +1,30 @@
+# __BEGIN_LICENSE__
+# Copyright (C) 2008-2010 United States Government as represented by
+# the Administrator of the National Aeronautics and Space Administration.
+# All Rights Reserved.
+# __END_LICENSE__
+
+from fnmatch import fnmatch
+
+from gevent.queue import Queue
+
+
+class QueueRouter(object):
+    def __init__(self):
+        self._subs = {}
+
+    def hasSubscribers(self):
+        return self._subs != {}
+
+    def subscribe(self, topicPattern):
+        q = Queue()
+        self._subs[id(q)] = (topicPattern, q)
+        return q
+
+    def unsubscribe(self, q):
+        del self._subs[id(q)]
+
+    def publish(self, topic, msg):
+        for subscriberId, (topicPattern, q) in self._subs.iteritems():
+            if fnmatch(topic, topicPattern):
+                q.put((topic, msg))
---
+++ geocamUtil/gevent/testQrouter.py
@@ -0,0 +1,63 @@
+# __BEGIN_LICENSE__
+# Copyright (C) 2008-2010 United States Government as represented by
+# the Administrator of the National Aeronautics and Space Administration.
+# All Rights Reserved.
+# __END_LICENSE__
+
+import unittest
+import logging
+import itertools
+
+import gevent
+from gevent.event import AsyncResult
+
+from qrouter import QueueRouter
+
+
+def firstNElementsOf(it, n):
+    return list(itertools.islice(it, 0, n))
+
+
+def subscriber1(router, resultSlot):
+    q = router.subscribe('foo*')
+    resultSlot.set(firstNElementsOf(q, 3))
+        
+
+def subscriber2(router, resultSlot):
+    q = router.subscribe('f*')
+    result = []
+    with gevent.Timeout(0.05, False):
+        for elt in q:
+            result.append(elt)
+            if len(result) >= 3:
+                router.unsubscribe(q)
+    resultSlot.set(result)
+
+
+def publisher(router):
+    for i in xrange(5):
+        router.publish('foobar', i)
+        router.publish('baz', i)
+        gevent.sleep(0)
+
+
+class TestQueueRouter(unittest.TestCase):
+    def test_foo(self):
+        router = QueueRouter()
+        resultSlot1 = AsyncResult()
+        resultSlot2 = AsyncResult()
+        gevent.spawn(subscriber1, router, resultSlot1)
+        gevent.spawn(subscriber2, router, resultSlot2)
+        gevent.sleep(0)  # yield to subscribers for setup
+        gevent.spawn(publisher, router)
+        result1 = resultSlot1.wait()
+        result2 = resultSlot2.wait()
+        expected = [('foobar', 0),
+                    ('foobar', 1),
+                    ('foobar', 2)]
+        self.assertEqual(result1, expected)
+        self.assertEqual(result2, expected)
+
+
+if __name__ == '__main__':
+    unittest.main()
