4f6e68623708a3b7d9c7648847bb03452fde2a73
Trey Smith <trey.smith@nasa.gov>
2014-01-20 19:36:55 -0800

    add installgithooks management command

---
+++ geocamUtil/management/commands/installgithooks.py
@@ -0,0 +1,66 @@
+# __BEGIN_LICENSE__
+# Copyright (C) 2008-2010 United States Government as represented by
+# the Administrator of the National Aeronautics and Space Administration.
+# All Rights Reserved.
+# __END_LICENSE__
+
+import os
+import sys
+import glob
+from optparse import make_option
+
+from django.core.management.base import BaseCommand
+
+from geocamUtil.management.commandUtil import getSiteDir
+
+
+def dosys(cmd, verbosity):
+    if verbosity > 1:
+        print >> sys.stderr, cmd
+    ret = os.system(cmd)
+    if ret != 0:
+        if verbosity > 1:
+            print >> sys.stderr, 'command exited with non-zero return value %s' % ret
+    return ret
+
+
+class Command(BaseCommand):
+    help = 'Install git hooks from geocamUtil/management/githooks'
+
+    option_list = BaseCommand.option_list + (
+        make_option('-f', '--force',
+                    action='store_true',
+                    default=False,
+                    help='Overwrite any existing hooks'),
+    )
+
+    def handle(self, *args, **options):
+        verbosity = int(options.get('verbosity', 0))
+        force = options.get('force', False)
+
+        siteDir = getSiteDir()
+        srcDir = os.path.join(siteDir, 'apps', 'geocamUtil', 'management', 'githooks')
+        hookFiles = os.listdir(srcDir)
+
+        dotGitDirs = ['%s.git' % siteDir] + glob.glob('%s.git/modules/submodules/*' % siteDir)
+
+        findCmd = 'find %s -type d -name hooks | grep ".git/hooks"' % siteDir
+        if verbosity > 1:
+            print >> sys.stderr, 'running: %s' % findCmd
+        tgtDirs = os.popen(findCmd).read().splitlines()
+
+        for dotGitDir in dotGitDirs:
+            tgtDir = os.path.join(dotGitDir, 'hooks')
+            if not os.path.exists(tgtDir):
+                dosys('mkdir -p %s' % tgtDir, verbosity)
+            srcRelTgt = os.path.relpath(os.path.realpath(srcDir),
+                                        os.path.realpath(tgtDir))
+            for f in hookFiles:
+                tgtPath = os.path.join(tgtDir, f)
+                if os.path.exists(tgtPath):
+                    if force:
+                        dosys('rm -f %s' % tgtPath, verbosity)
+                    else:
+                        continue
+                srcPath = os.path.join(srcRelTgt, f)
+                dosys('ln -s %s %s' % (srcPath, tgtPath), verbosity)
