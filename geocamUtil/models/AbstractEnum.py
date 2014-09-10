154a3ecbee4efd9e9d0a3f0ee03d23871aa17a01
Tamar Cohen <tamar.e.cohen@nasa.gov>
2014-09-10 12:40:35 -0700

    order abstract enum by display_name

--- geocamUtil/models/AbstractEnum.py
+++ geocamUtil/models/AbstractEnum.py
@@ -7,6 +7,7 @@ from django.db import models
 class AbstractEnumModel(models.Model):
     class Meta:
         abstract = True
+        ordering = ['display_name']
     value = models.CharField(max_length=256)
     display_name = models.CharField(max_length=256, blank=True, null=True)
 
