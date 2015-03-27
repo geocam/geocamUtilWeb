bd22beb571aace74f302aeed7cb51b4750c77c03
Tamar Cohen <tamar.e.cohen@nasa.gov>
2015-03-27 15:40:35 -0700

    updated licenses

--- doc_src/reference/index.rst
+++ doc_src/reference/index.rst
@@ -12,7 +12,17 @@ Reference
    settings
 
 .. o  __BEGIN_LICENSE__
-.. o  Copyright (C) 2008-2010 United States Government as represented by
-.. o  the Administrator of the National Aeronautics and Space Administration.
-.. o  All Rights Reserved.
+.. o Copyright © 2015, United States Government, as represented by the 
+.. o Administrator of the National Aeronautics and Space Administration. 
+.. o All rights reserved.
+.. o 
+.. o The xGDS platform is licensed under the Apache License, Version 2.0 
+.. o (the "License"); you may not use this file except in compliance with the License. 
+.. o You may obtain a copy of the License at 
+.. o http://www.apache.org/licenses/LICENSE-2.0.
+.. o 
+.. o Unless required by applicable law or agreed to in writing, software distributed 
+.. o under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR 
+.. o CONDITIONS OF ANY KIND, either express or implied. See the License for the 
+.. o specific language governing permissions and limitations under the License.
 .. o  __END_LICENSE__
--- geocamUtil/bin/relicense.pl
+++ geocamUtil/bin/relicense.pl
@@ -1,8 +1,10 @@
 #!/usr/bin/perl
 # __BEGIN_LICENSE__
-# Copyright (C) 2008-2010 United States Government as represented by
-# the Administrator of the National Aeronautics and Space Administration.
-# All Rights Reserved.
+# Copyright © 2015, United States Government, as represented by the Administrator of the National Aeronautics and Space Administration. All rights reserved.
+#
+# The “xGDS” platform is licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0.
+#
+# Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
 # __END_LICENSE__
 
 
@@ -17,6 +19,7 @@ my %comment = (
     ".am"   => "#",
     ".cc"   => "//",
     ".cpp"  => "//",
+    ".css"  => "/*",
     ".cxx"  => "//",
     ".oldtest"  => "//",
     ".hpp"  => "//",
@@ -40,6 +43,8 @@ my %comment = (
 
 my %atEnd = (
     ".rst" => 1,
+    ".css"  => "*/",
+    
 );
 
 # Read the license text from __DATA__ by default
@@ -116,7 +121,17 @@ foreach my $filename (<>) {
 
 __DATA__
  __BEGIN_LICENSE__
- Copyright (C) 2008-2010 United States Government as represented by
- the Administrator of the National Aeronautics and Space Administration.
- All Rights Reserved.
+Copyright © 2015, United States Government, as represented by the 
+Administrator of the National Aeronautics and Space Administration. 
+All rights reserved.
+
+The xGDS platform is licensed under the Apache License, Version 2.0 
+(the "License"); you may not use this file except in compliance with the License. 
+You may obtain a copy of the License at 
+http://www.apache.org/licenses/LICENSE-2.0.
+
+Unless required by applicable law or agreed to in writing, software distributed 
+under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR 
+CONDITIONS OF ANY KIND, either express or implied. See the License for the 
+specific language governing permissions and limitations under the License.
  __END_LICENSE__
