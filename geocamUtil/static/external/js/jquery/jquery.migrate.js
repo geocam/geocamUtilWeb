f50e2b43bdb9dd702d7cdf7e197a26af9e723dbd
Tamar Cohen <tamar.e.cohen@nasa.gov>
2015-03-31 12:46:52 -0700

    truly truly corrected license 2015 NOSA

--- geocamUtil/static/external/js/jquery/jquery.migrate.js
+++ geocamUtil/static/external/js/jquery/jquery.migrate.js
@@ -1,3 +1,9 @@
+// __BEGIN_LICENSE__
+// Copyright (C) 2008-2010 United States Government as represented by
+// the Administrator of the National Aeronautics and Space Administration.
+// All Rights Reserved.
+// __END_LICENSE__
+
 /*!
  * jQuery Migrate - v1.2.1 - 2013-05-08
  * https://github.com/jquery/jquery-migrate
--- geocamUtil/static/geocamUtil/js/datatable_helpers.js
+++ geocamUtil/static/geocamUtil/js/datatable_helpers.js
@@ -1,3 +1,9 @@
+// __BEGIN_LICENSE__
+// Copyright (C) 2008-2010 United States Government as represented by
+// the Administrator of the National Aeronautics and Space Administration.
+// All Rights Reserved.
+// __END_LICENSE__
+
 /*
  * Utilities to make using datatable easier
  * Import it of course
