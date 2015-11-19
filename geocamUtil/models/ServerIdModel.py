85794c0ea1ee786c28990aead707531a0a4f1c59
Tamar Cohen <tamar.e.cohen@nasa.gov>
2015-11-19 15:10:40 -0800

    serverId with unique id using memcached

--- geocamUtil/models/ServerIdModel.py
+++ geocamUtil/models/ServerIdModel.py
@@ -14,43 +14,81 @@
 # specific language governing permissions and limitations under the License.
 # __END_LICENSE__
 
+import time
+
+from django.core.cache import cache
 from django.db import models
+from django.db import connection
+
 from geocamUtil.defaultSettings import HOSTNAME
 
-index_map = {}
 
 class ServerIdModel(models.Model):
+    ''' This abstract class generates unique character ids of the format servername_index, ie myserver_1, myserver_2.
+    If you are synchronizing between multiple servers, it will find the largest index and add one.
+    It makes use of memcached, so you must have:
+    (in python)
+    pylibmc
+    (in ubuntu)
+    memcached
+    libmemcached-dev
+    
+    This uses memcached to store the largest index and to keep a semaphore for usage of the memcache.  It tries only 10 times for the key.
+    '''
+    
     # custom id field for uniqueness
     id = models.CharField(max_length=128,
                           unique=True, blank=False,
                           editable=False, primary_key=True)
     
     @classmethod
-    def getSafeIndex(cls, index):
-        pk_test = HOSTNAME + "_" + str(index)
+    def getUniqueIndex(cls):
+        query = "select MAX(CAST(SUBSTRING_INDEX(id, '_', -1) AS UNSIGNED))  from " + cls._meta.db_table
+        cursor = connection.cursor()
+        cursor.execute(query)
+        result = cursor.fetchone()
         try:
-            cls.objects.get(pk=pk_test)
-            return cls.getSafeIndex(index + 1)
+            index = int(result[0]) + 1
         except:
-            return index
-
+            index = 1
+        finally:
+            cursor.close()
+        return index
+    
     @classmethod
-    def getLastId(cls, index=None):
-        if not index:
-            try:
-                index = index_map[cls.__name__]
-                index = index + 1
-                index_map[cls.__name__] = index
-            except:
-                # Initializing
-                if cls.objects.count():
-                    index = cls.objects.count() + 1
-                else:
-                    index = 0
-                index = cls.getSafeIndex(index)
-                index_map[cls.__name__] = index
+    def getKey(cls):
+        keyName = "using_" + str(cls.__name__)
+        key = cache.get(keyName)
+        if not key:
+            cache.set(keyName, True)
+        else:
+            count = 0
+            # now we have to wait
+            while key and count<10:
+                time.sleep(0.001)
+                key = cache.get(keyName)
+                count = count + 1
+            if not key:
+                cache.set("using " + cls.__name__, True)
+            else:
+                raise Exception("Could not get key to insert into " + cls.__name__)
+        return keyName
+        
+    @classmethod
+    def getLastId(cls):
+        # get the key to use this 
+        keyName = cls.getKey()
             
-            return index
+        try:
+            index = cache.get(cls.__name__)
+            index = index + 1
+        except:
+            # Initializing
+            index = cls.getUniqueIndex()
+        cache.set(cls.__name__,index)
+        cache.set(keyName, None)
+            
+        return index
 
     def fillId(self):
         next_id = self.getLastId()
