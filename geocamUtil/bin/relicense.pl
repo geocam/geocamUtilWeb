dbd9b102cff0a71a5ad97bd16c19901f1e89f659
Tamar Cohen <tamar.e.cohen@nasa.gov>
2015-11-30 21:08:30 +0000

    updated license fixed spaces

--- geocamUtil/bin/relicense.pl
+++ geocamUtil/bin/relicense.pl
@@ -119,31 +119,19 @@ foreach my $filename (<>) {
     write_file($filename, $file);
 }
 
-#__BEGIN_SIMPLE_LICENSE__
-#Copyright (c) 2015, United States Government, as represented by the
-#Administrator of the National Aeronautics and Space Administration.
-#All rights reserved.
-#__END_SIMPLE_LICENSE__
-
-# __BEGIN_LICENSE__
-#Copyright (c) 2015, United States Government, as represented by the 
-#Administrator of the National Aeronautics and Space Administration. 
-#All rights reserved.
-#
-#The xGDS platform is licensed under the Apache License, Version 2.0 
-#(the "License"); you may not use this file except in compliance with the License. 
-#You may obtain a copy of the License at 
-#http://www.apache.org/licenses/LICENSE-2.0.
-#
-#Unless required by applicable law or agreed to in writing, software distributed 
-#under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR 
-#CONDITIONS OF ANY KIND, either express or implied. See the License for the 
-#specific language governing permissions and limitations under the License.
-# __END_LICENSE__
- 
 __DATA__
- __BEGIN_LICENSE__
-Copyright (c) 2015, United States Government, as represented by the 
-Administrator of the National Aeronautics and Space Administration. 
-All rights reserved.
- __END_LICENSE__
+__BEGIN_LICENSE__
+ Copyright (c) 2015, United States Government, as represented by the
+ Administrator of the National Aeronautics and Space Administration.
+ All rights reserved.
+
+ The xGDS platform is licensed under the Apache License, Version 2.0
+ (the "License"); you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+ http://www.apache.org/licenses/LICENSE-2.0.
+
+ Unless required by applicable law or agreed to in writing, software distributed
+ under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+ CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ specific language governing permissions and limitations under the License.
+__END_LICENSE__
