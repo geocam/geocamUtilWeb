d0a396c664481228aa847da9f38770ceaf969f40
Tamar Cohen <tamar.cohen@nasa.gov>
2016-03-30 15:45:52 -0700

    added copyright

--- geocamUtil/static/geocamUtil/js/packem.js
+++ geocamUtil/static/geocamUtil/js/packem.js
@@ -1,3 +1,9 @@
+// __BEGIN_LICENSE__
+//Copyright (c) 2015, United States Government, as represented by the 
+//Administrator of the National Aeronautics and Space Administration. 
+//All rights reserved.
+// __END_LICENSE__
+
 function initializePackery() {
     //update  sizing
     var $container = $('#container');
