0dc3c944cf69fdc7bf1b662fc5a1f5719e6ed171
Tamar Cohen <tamar.cohen@nasa.gov>
2016-04-05 16:03:02 -0700

    removed packery

--- geocamUtil/static/geocamUtil/js/datatable_helpers.js
+++ geocamUtil/static/geocamUtil/js/datatable_helpers.js
@@ -133,3 +133,34 @@ function connectSelectionCallback(table, callback, singleSelection){
     }
 
 }
+
+/* 
+ * Table View
+ */
+function setupTable(divID, tableID, initialData, aoColumns){
+	// initialize the table with json of existing data.
+	defaultOptions["aaData"] = initialData;
+	defaultOptions["aoColumns"] = aoColumns;
+	defaultOptions["scrollY"] = 200;
+	defaultOptions["fnCreatedRow"] = function(nRow, aData, iDataIndex) { // add image id to row
+		$(nRow).attr('id', aData['id'])
+	}
+	
+	if ( ! $.fn.DataTable.isDataTable( '#' + tableID ) ) {
+		  theDataTable = $('#' + tableID ).dataTable(defaultOptions);
+	}
+	
+	// handle resizing
+	var tableResizeTimeout;
+	$('#' + divID).resize(function() {
+	    // debounce
+	    if ( tableResizeTimeout ) {
+		clearTimeout( tableResizeTimeout );
+	    }
+
+	    tableResizeTimeout = setTimeout( function() {
+		updateTableScrollY(divID, tableID);
+	    }, 30 );
+	});
+	
+}
