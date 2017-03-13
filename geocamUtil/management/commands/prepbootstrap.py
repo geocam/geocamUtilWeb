39c9127a274abac4bbb24e46ed040d0cde205516
Tamar Cohen <tamar.cohen@nasa.gov>
2017-03-13 12:54:19 -0700

    updates for bootstrap4 and to jquery 3.1.1

---
+++ geocamUtil/management/commands/prepbootstrap.py
@@ -0,0 +1,39 @@
+# __BEGIN_LICENSE__
+#Copyright (c) 2015, United States Government, as represented by the 
+#Administrator of the National Aeronautics and Space Administration. 
+#All rights reserved.
+# __END_LICENSE__
+
+import os
+import sys
+import django
+
+from optparse import make_option
+
+from django.core.management.base import BaseCommand
+
+from geocamUtil.management.commandUtil import getSiteDir, dosys
+from django.conf import settings
+
+"""
+Call grunt to compile bootstrap 4
+"""
+class Command(BaseCommand):
+    help = 'Use grunt to compile css from sass in various directories.  Node, Ruby, grunt-cli, bundler must be installed.'
+
+    def handle(self, *args, **options):
+        verbosity = int(options.get('verbosity', 0))
+
+        siteDir = getSiteDir()
+        bootstrapDir = getattr(settings, 'STATIC_ROOT', '%sbuild/static/bootstrap' % siteDir)
+
+        ret = os.chdir(bootstrapDir)
+        if ret != 0:
+            if verbosity > 1:
+                print >> sys.stderr, ret
+        if verbosity > 1:
+            print 'about to grunt bootstrap'
+        dosys('grunt dist ', verbosity)
+        if verbosity > 1:
+            print 'done with grunt for bootstrap'
+
