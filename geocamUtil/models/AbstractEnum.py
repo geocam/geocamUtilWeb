f3a655103aa7f7196fdcc072bf3b9ead24f39427
Tamar Cohen <tamar.cohen@nasa.gov>
2017-04-27 17:11:53 -0700

    added enum manager

--- geocamUtil/models/AbstractEnum.py
+++ geocamUtil/models/AbstractEnum.py
@@ -9,12 +9,24 @@ from django.db import models
 # pylint: disable=C1001
 
 
+class EnumManager(models.Manager):
+    def get_by_natural_key(self, value, display_name):
+        return self.get(value=value, display_name=display_name)
+
+
 class AbstractEnumModel(models.Model):
-    class Meta:
-        abstract = True
-        ordering = ['display_name']
-    value = models.CharField(max_length=256)
+    objects = EnumManager()
+    
+    value = models.CharField(max_length=128, unique=True)
     display_name = models.CharField(max_length=256, blank=True, null=True)
+
+    def natural_key(self):
+        return (self.value, self.display_name)
 
     def __unicode__(self):
         return self.display_name or self.value
+    
+    class Meta:
+        abstract = True
+        ordering = ['display_name']
+
