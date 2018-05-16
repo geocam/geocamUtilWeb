e87256f017d9e72a4a3f4448a05a05e5a0d5725d
Tamar Cohen <tamar.cohen@nasa.gov>
2018-05-16 14:48:29 -0700

    added lazy manager and lazy query set which are not yet used

--- geocamUtil/models/managers.py
+++ geocamUtil/models/managers.py
@@ -9,6 +9,7 @@ from operator import attrgetter
 
 from django.db import models
 import django.core.exceptions
+from django.conf import settings
 
 from geocamUtil.loader import getModelByName
 
@@ -213,3 +214,125 @@ class LazyModelCollectionManager(AbstractModelManager):
 
     def registerChildClass(self, cls):
         self._childClasses.append(cls)
+
+
+class LazyQuerySet(models.QuerySet):
+    """ A manager for a model that is lazy loading, and takes the name of the variable that
+        will hold the class name.  When the query set is demanded, initialization will be run (once)
+        Note this seems useful but we are not using it yet.
+        """
+
+    def __init__(self, model_name_variable=None, query=None, using=None, hints=None, model=None):
+        """
+        Initialize with either the model or the model name variable.
+        :param model_name_variable: the name of the variable holding the model name, ie 'settings.bla'
+        :param query:
+        :param using:
+        :param hints:
+        :param model: This is just used when cloning or other internal uses, or if you already have the model.
+        """
+        if not model_name_variable and not model:
+            raise Exception('You must define the model name variable or the model')
+        if model:
+            self.initialized = True
+            super(LazyQuerySet, self).__init__(model, query, using, hints)
+            return
+
+        self.name = model_name_variable.split('.')[-1]
+        self.model_name_variable = model_name_variable
+        self.model = model
+        self.initial_query = query
+        self.initial_using = using
+        self.initial_hints = hints
+        self.initialized = False
+
+    def do_initialization(self):
+        """ actually do the initialization and look up the models """
+        if not self.initialized:
+            model_name = eval(self.model_name_variable)
+            self.model = getModelByName(model_name)
+            super(LazyQuerySet, self).__init__(self.model, self.initial_query, self.initial_using, self.initial_hints)
+            self.initialized = True
+
+    def __iter__(self):
+        self.do_initialization()
+        return super(LazyQuerySet, self).__iter__()
+
+    def __len__(self):
+        self.do_initialization()
+        return super(LazyQuerySet, self).__len__()
+
+    def __repr__(self):
+        self.do_initialization()
+        return super(LazyQuerySet, self).__repr__()
+
+    def __getitem__(self, k):
+        self.do_initialization()
+        return super(LazyQuerySet, self).__getitem__(k)
+
+    def _clone(self):
+        self.do_initialization()
+        result = super(LazyQuerySet, self)._clone()
+        result.do_initialization()
+        return result
+
+    def filter(self, *args, **kwargs):
+        self.do_initialization()
+        return super(LazyQuerySet, self).filter(*args, **kwargs)
+
+    def exclude(self, *args, **kwargs):
+        self.do_initialization()
+        return super(LazyQuerySet, self).exclude(*args, **kwargs)
+
+    def distinct(self, true_or_false=True):
+        self.do_initialization()
+        return super(LazyQuerySet, self).distinct(true_or_false)
+
+    def order_by(self, *field_names):
+        self.do_initialization()
+        return super(LazyQuerySet, self).order_by(*field_names)
+
+    def select_related(self, *fields, **kwargs):
+        self.do_initialization()
+        return super(LazyQuerySet, self).select_related(*fields, **kwargs)
+
+    def get(self, *args, **kwargs):
+        self.do_initialization()
+        return super(LazyQuerySet, self).get(*args, **kwargs)
+
+    def count(self):
+        self.do_initialization()
+        return super(LazyQuerySet, self).count()
+
+    def delete(self):
+        self.do_initialization()
+        return super(LazyQuerySet, self).delete()
+
+    def _fetch_all(self):
+        self.do_initialization()
+        return super(LazyQuerySet, self)._fetch_all()
+
+
+class LazyModelManager(models.Manager):
+
+    """ A manager for a model that is lazy loading, and takes the name of the variable that
+    will hold the class name.  When the query set is demanded, initialization will be run (once)
+    TODO: untested
+    """
+    def __init__(self, model_name_variable):
+        self.name = model_name_variable.split('.')[-1]
+        self.model_name_variable = model_name_variable
+        self.initialized = False
+
+    def do_initialization(self):
+        """ actually do the initialization and look up the models """
+        if not self.initialized:
+            model_name = eval(self.model_name_variable)
+            self.model = getModelByName(model_name)
+            super(LazyModelManager, self).__init__()
+
+            self.initialized = True
+
+    def get_queryset(self):
+        self.do_initialization()
+        return super(LazyModelManager, self).get_queryset()
