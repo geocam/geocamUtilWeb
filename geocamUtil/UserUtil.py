32106e95f98fec6a396af1c9298b77ff7ae43bda
Tamar Cohen <tamar.cohen@nasa.gov>
2018-09-13 15:55:45 -0700

    updated userutil

--- geocamUtil/UserUtil.py
+++ geocamUtil/UserUtil.py
@@ -4,6 +4,8 @@
 #All rights reserved.
 # __END_LICENSE__
 
+from django.contrib.auth.models import User
+
 def getUserName(user):
     if not user:
         return ''
@@ -13,3 +15,11 @@ def getUserName(user):
         if user.last_name:
             username = username + " " + user.last_name
     return username
+
+
+def getUserByNames(first, last):
+    return User.objects.get(first_name=first, last_name=last)
+
+
+def getUserByUsername(username):
+    return User.objects.get(username=username)
